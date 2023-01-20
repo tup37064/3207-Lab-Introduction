b2e40ca5c031f9925258f640b7e0611e87c956e9
Shawn Dutill <shawn.dutill@temple.edu>
2023-01-20 16:46:32 -0500

    working version

--- print_random.c
+++ print_random.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-
+extern char randchar();
 
 int main()
 {
--- random.c
+++ random.c
@@ -1,1 +1,9 @@
-Shit
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+char randchar(){
+//	printf("Generating random letter\n");
+	int r = rand() % (90-65+1)+65;      // Returns a pseudo-random integer between 0 and RAND_MAX
+	return (char)r;
+}
